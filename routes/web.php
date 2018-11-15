6bc61e7120902a6a063e68a098bf7dc2ac065c99
kbsg <kebryansg@gmail.com>
2018-11-15 16:26:56 -0500

    Midd

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->group(['middleware' => [ 'auth', 'valid' ] ], function () use ($router) {
+
+});
